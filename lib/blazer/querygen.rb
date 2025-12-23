ee8f29578a5bc76863255d325651ce1cc0ff9f77
mat_aki <mat_aki@sonicgarden.jp>
2025-12-23 12:13:43 +0900

    Init

---
+++ lib/blazer/querygen.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "querygen/version"
+
+module Blazer
+  module Querygen
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
